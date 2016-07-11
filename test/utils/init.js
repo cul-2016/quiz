43cbc06a1b8b0dc0a1b9efa0a379c6cd2aac9b9c
sohilpandya <sohilpandya@me.com>
2016-07-11 13:23:08 +0100

    sets up initial schema and db test #26

--- test/utils/init.js
+++ test/utils/init.js
@@ -1,2 +1,6 @@
 import Server from '../../server/server.js';
 export const server = Server.init(process.env.PORT);
+import pg from 'pg';
+
+export const testClient = new pg.Client({ database: 'testing' });
+// end to end testing => pool and testing pg client need to refer to the same one when testing endpoints!
