47cda2796ab4c8022958eaa4b07a549ca31d213c
sohilpandya <sohilpandya@me.com>
2016-07-11 13:23:08 +0100

    sets up initial schema and db test #26

--- server/lib/init-db.js
+++ server/lib/init-db.js
@@ -4,12 +4,18 @@ var Pool = require('pg').Pool;
 process.on('unhandledRejection', (e) => {
     console.error(e.message, e.stack);
 });
-
-var db_pool = new Pool({ database: 'testing', idleTimeoutMillis: 2000 });
+let db_url = process.env.TESTING ? 'testing' : 'remote-db-url';
+const db_pool = new Pool({ database: db_url, idleTimeoutMillis: 2000 });
 
 db_pool.connect((error, client, done) => {
 
-    fs.readFile(__dirname + '/schema.txt', 'utf8', (error, schema) => {
+    let path_to_schema = process.env.TESTING ? '/test-schema.txt' : 'path-to-proper-schema';
+
+    fs.readFile(__dirname + path_to_schema, 'utf8', (error, schema) => {
+
+        if (error) {
+            throw new Error("Error reading file:", error);
+        }
 
         client.query(schema, (error, result) => {
             if (error) {
--- test/integration/database.js
+++ test/integration/database.js
@@ -1,6 +1,21 @@
 import test from 'tape';
+import { testClient } from '../utils/init';
+import { users } from '../utils/database-fixtures';
 
 test('database works', (t) => {
 
-    t.end();
+    t.plan(2);
+    testClient.connect( (error) => {
+        if (error) {
+            console.error(error);
+        }
+        testClient.query('SELECT * from users WHERE user_id = 1', (error, response) => {
+            if (error) {
+                console.error(error, 'from client query');
+            }
+            t.equal(response.rowCount, 1, 'Query matches one row');
+            t.deepEqual(response.rows[0], users[0], 'Correct user retrieved');
+            testClient.end();
+        });
+    });
 });
