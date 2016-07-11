3fe7f681e94b7febb3b38c4c002b0291b9c019ea
sohilpandya <sohilpandya@me.com>
2016-07-11 13:25:32 +0100

    adds error handling for unset env variables #26

--- test/utils/init.js
+++ test/utils/init.js
@@ -2,5 +2,9 @@ import Server from '../../server/server.js';
 export const server = Server.init(process.env.PORT);
 import pg from 'pg';
 
+if (!process.env.TESTING) {
+    throw new Error('Please set up the testing environment variables');
+}
+
 export const testClient = new pg.Client({ database: 'testing' });
 // end to end testing => pool and testing pg client need to refer to the same one when testing endpoints!
