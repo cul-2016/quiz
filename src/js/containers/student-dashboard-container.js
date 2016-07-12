5e0e5ed576b2ed5174409ae4163a30ac6d0e777e
Mina <wilhelmina.jane@gmail.com>
2016-07-12 11:58:15 +0100

    adds documentation comments #21 and adds preliminary dashboard components

---
+++ src/js/containers/student-dashboard-container.js
@@ -0,0 +1,7 @@
+import { connect } from 'react-redux';
+import StudentDashboard from '../components/student/dashboard';
+
+
+const StudentDashboardContainer = connect()(StudentDashboard);
+
+export default StudentDashboardContainer;
--- src/js/lib/validCookieExists.js
+++ src/js/lib/validCookieExists.js
@@ -1,3 +1,7 @@
+/**
+ * Checks for the existence of a valid cookie
+ */
+
 export default function validCookieExists () {
 
     return document.cookie.indexOf("quizID") !== -1;
