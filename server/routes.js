bb2a209abe624df57bb34bb48d2ddc692f89d59c
sohilpandya <sohilpandya@me.com>
2016-07-15 16:55:47 +0100

    #41 finished endpoint for registering and signing in. backend is in place for this functionality to work

--- server/routes.js
+++ server/routes.js
@@ -2,7 +2,7 @@ var Index = require('./routes/index');
 var GetModules = require('./routes/get-modules');
 var ValidateModule = require('./routes/validate-module');
 var SaveUser = require('./routes/save-user');
-var ValidateUser = require('./routes/validate-user');
+var ValidateUser = require('./routes/authenticate-user');
 
 var routes = [
     Index,
