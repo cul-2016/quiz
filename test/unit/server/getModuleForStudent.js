53d47f37f2144cfdb2e1569c73c137758091c55d
sohilpandya <sohilpandya@me.com>
2016-08-25 11:24:46 +0100

    #128 fixed line spacing for exected objects in getModuleForStudent and Lecturer tests"

--- test/unit/server/getModuleForStudent.js
+++ test/unit/server/getModuleForStudent.js
@@ -5,7 +5,19 @@ import { testClient } from '../../utils/init';
 test('Gets the module for a given student', (t) => {
 
     t.plan(1);
-    const expectedRows = { medals: { condition: [39, 69], medal_name: ['bronze', 'silver', 'gold'] }, module_id: 'TEST', name: 'test module', trophies_awarded: { first_quiz: false, high_score: false, overall_average: false, participation: false } };
+    const expectedRows = {
+        medals: {
+            condition: [39, 69],
+            medal_name: ['bronze', 'silver', 'gold']
+        },
+        module_id: 'TEST',
+        name: 'test module',
+        trophies_awarded: {
+            first_quiz: false,
+            high_score: false,
+            overall_average: false,
+            participation: false 
+        } };
 
     getModuleForStudent(testClient, 'TEST', 1, (error, response) => {
 
