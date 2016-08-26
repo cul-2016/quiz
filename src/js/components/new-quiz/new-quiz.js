99d3bfa661e461eb6014c4f4a6ece7071c907adb
sohilpandya <sohilpandya@me.com>
2016-08-26 15:13:24 +0100

    added classname validation so that the user is unable to save the quiz if the quiz name or any fields in the questions are empty #48

--- src/js/components/new-quiz/new-quiz.js
+++ src/js/components/new-quiz/new-quiz.js
@@ -1,8 +1,27 @@
 import React, { PropTypes } from 'react';
 import Questions from './questions';
 import Nav from '../general/nav';
+import classnames from 'classnames';
 
 const NewQuiz = ({ newQuiz, handleAddQuestion, handleInputChange, handleQuizNameChange, handleSaveQuiz, location, username }) => {
+
+    const questionsValidation = newQuiz.questions.map((question) => {
+        if (!question.question || !question.a || !question.b || !question.c || !question.d || !question.correct_answer){
+            return false;
+        } else {
+            return true;
+        }
+    }).every((elem) => {
+        return elem;
+    });
+
+    const submitClasses = classnames("button is-success save-question", {
+        "is-disabled": !newQuiz.name || questionsValidation === false
+    });
+    const quizNameClasses = classnames("help is-danger", {
+        "display-none": newQuiz.name
+    });
+
     return (
             <div>
                 <Nav username={ username } />
@@ -17,6 +36,8 @@ const NewQuiz = ({ newQuiz, handleAddQuestion, handleInputChange, handleQuizName
                         defaultValue={ newQuiz.name }
                         onChange={ (e) => handleQuizNameChange(e.target.value) }
                         placeholder='Quiz Name'></input>
+                        <span className={ quizNameClasses }>Please enter a Quiz Name</span>
+
                 </div>
 
                 <Questions
@@ -27,7 +48,7 @@ const NewQuiz = ({ newQuiz, handleAddQuestion, handleInputChange, handleQuizName
                     <button className="button is-info add-question" onClick={ handleAddQuestion }>
                         Add Question
                     </button>
-                    <button className="button is-success save-question" onClick={ () => handleSaveQuiz(location.pathname.split('/')[1], newQuiz.name, newQuiz.questions) }>
+                    <button className={ submitClasses } onClick={ () => handleSaveQuiz(location.pathname.split('/')[1], newQuiz.name, newQuiz.questions) }>
                         Save and Exit
                     </button>
                 </div>
