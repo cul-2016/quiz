f2df57a3e95d657261d9cee1da4a456f2e73bd28
Mina <wilhelmina.jane@gmail.com>
2016-09-10 18:41:15 +0100

    removes unnecessary transpiling

--- server/lib/getBestAndWorstQuiz.js
+++ server/lib/getBestAndWorstQuiz.js
@@ -1,6 +1,3 @@
-require('babel-register')({
-    presets: ['es2015', 'stage-0']
-});
 var getMeanQuizScores = require('./getMeanQuizScores');
 var getQuizScores = require('./getQuizScores');
 var getSignedDifference = require('./getSignedDifference');
