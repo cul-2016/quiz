a74b974c7ab4370308fc3911f30dd9a39578c9a7
Mina <wilhelmina.jane@gmail.com>
2016-09-15 14:30:54 +0100

    arranges student history into a table #122

--- src/js/components/student-module/history.js
+++ src/js/components/student-module/history.js
@@ -5,20 +5,32 @@ const StudentHistory = ({ history }) => {
 
     let mappedHistory = history.map((quiz, i) => {
         return (
-            <div key={ i }>
-                <div className="box average">
-                    <span>{ quiz.name }</span>
-                    <span>Score: { quiz.score }</span>
-                    <span>out of: { quiz.num_questions }</span>
-                </div>
-            </div>
+            <tr key={ i }>
+                <td className="subtitle is-4">
+                    { quiz.name }
+                </td>
+                <td>
+                    <span className="subtitle is-3">
+                        { `${quiz.score} ` }
+                    </span>
+                    <span>
+                        { `out of ${quiz.num_questions}` }
+                    </span>
+                </td>
+                <td>
+                    The medal
+                </td>
+            </tr>
         );
     });
 
     return (
-        <div>
-            { mappedHistory }
-        </div>
+        <table className="student-history table">
+            <tbody>
+
+                { mappedHistory }
+            </tbody>
+        </table>
     );
 };
 
