ca9a06e46848240afe5df549fd476a0b2eb8f56f
sohilpandya <sohilpandya@me.com>
2016-09-20 18:24:58 +0100

    more sharing buttons

--- src/js/components/dashboard.js
+++ src/js/components/dashboard.js
@@ -43,6 +43,8 @@ class Dashboard extends React.Component {
                 <Nav username={ this.props.username } />
                 <h2 className="has-text-centered"> Modules </h2>
                 <a href="whatsapp://send?text=The text to share!" data-action="share/whatsapp/share">Share via Whatsapp</a>
+                <a href="fb://friends" >Share via facebook</a>
+                <a href="sms:">Share via messages</a>
                 <div className="container">
                     {
                         this.props.is_lecturer &&
