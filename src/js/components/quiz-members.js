d989807f9d09217c20f3520e61ff147f0cb356d8
Mina <wilhelmina.jane@gmail.com>
2016-09-22 15:24:16 +0100

    .#173 move quiz questions in quiz-members to modal

--- src/js/components/quiz-members.js
+++ src/js/components/quiz-members.js
@@ -1,67 +1,95 @@
-import React, { PropTypes } from 'react';
+import React, { PropTypes, Component } from 'react';
 import { Link } from 'react-router';
 import Spinner from './general/spinner';
-import QuizReviewQuestions from './review/quiz-review-questions';
+import QuizMembersModal from './quiz-members-modal';
 
 
-const QuizMembers = ({ members, isFetchingQuizMembers, questions, params }) => {
+class QuizMembers extends Component {
 
+    constructor (props) {
+        super(props);
+        this.state = {
+            isModalVisible: false
+        };
+        this.showQuizQuestions = this.showQuizQuestions.bind(this);
+        this.hideQuizQuestions = this.hideQuizQuestions.bind(this);
+    }
 
-    let mappedMembers = members.map((member, i) => {
-        return (
-            <div key={i} className="column is-8 is-offset-2 ">
-                <div className="columns quiz-members">
-                    <div className="column is-5 email">
-                        <p>{ member.email }</p>
-                    </div>
-                    <div className="column is-3 username">
-                        <p>{ member.username }</p>
-                    </div>
-                    <div className="column is-1 score">
-                        <p>{ member.score }</p>
-                    </div>
-                    <div className="column is-2 edit-score-button">
-                        <Link to={ `${params.module_id}/${params.quiz_id}/${i}/edit-score` }>
-                            <button className="button is-warning">Edit Score</button>
-                        </Link>
-                    </div>
-                </div>
-            </div>
-        );
-    });
+    showQuizQuestions () {
+        this.setState({
+            isModalVisible: true
+        });
+    }
 
-    return (
-        <div>
-        {
-            isFetchingQuizMembers && <Spinner />
-        }
-        {
-            !isFetchingQuizMembers && members &&
-            <div>
-                <QuizReviewQuestions questions = { questions }/>
-                <div>
-                    <h2 className="has-text-centered"> Quiz Members </h2>
-                    <div className="column is-8 is-offset-2">
-                        <div className="columns">
-                            <div className="column is-5">
-                                <label className="label">Email</label>
-                            </div>
-                            <div className="column is-3">
-                                <label className="label">Nickname</label>
-                            </div>
-                            <div className="column is-3">
-                                <label className="label">Score</label>
-                            </div>
+    hideQuizQuestions () {
+        this.setState({
+            isModalVisible: false
+        });
+    }
+
+    render () {
+
+        let { members, isFetchingQuizMembers, questions, params } = this.props;
+
+        let mappedMembers = members.map((member, i) => {
+            return (
+                <div key={i} className="column is-8 is-offset-2 ">
+                    <div className="columns quiz-members">
+                        <div className="column is-5 email">
+                            <p>{ member.email }</p>
+                        </div>
+                        <div className="column is-3 username">
+                            <p>{ member.username }</p>
+                        </div>
+                        <div className="column is-1 score">
+                            <p>{ member.score }</p>
+                        </div>
+                        <div className="column is-2 edit-score-button">
+                            <Link to={ `${params.module_id}/${params.quiz_id}/${i}/edit-score` }>
+                                <button className="button is-warning">Edit Score</button>
+                            </Link>
                         </div>
                     </div>
-                    { mappedMembers }
                 </div>
-            </div>
+            );
+        });
 
-        }
-        </div>
-    );
-};
+        return (
+            <div>
+                {
+                    isFetchingQuizMembers && <Spinner />
+                }
+                {
+                    !isFetchingQuizMembers && members &&
+                    <div className="quiz-members">
+                        <button className="button" onClick={ () => this.showQuizQuestions() }>
+                            Show quiz questions
+                        </button>
+                        <QuizMembersModal isVisible={ this.state.isModalVisible } questions={ questions } hide={ this.hideQuizQuestions }/>
+                        <div>
+                            <h2 className="has-text-centered"> Quiz Members </h2>
+                            <div className="column is-8 is-offset-2">
+                                <div className="columns">
+                                    <div className="column is-5">
+                                        <label className="label">Email</label>
+                                    </div>
+                                    <div className="column is-3">
+                                        <label className="label">Nickname</label>
+                                    </div>
+                                    <div className="column is-3">
+                                        <label className="label">Score</label>
+                                    </div>
+                                </div>
+                            </div>
+                            { mappedMembers }
+                        </div>
+                    </div>
+
+                }
+            </div>
+        );
+    }
+}
 
 QuizMembers.propTypes = {
     members: PropTypes.array,
