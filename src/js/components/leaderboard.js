98e9ae8fe7e27d55409ae090dcd9cda2d22ccc98
Mina <wilhelmina.jane@gmail.com>
2016-09-22 14:21:57 +0100

    #173 WIP aligning leaderboard header

--- src/js/components/leaderboard.js
+++ src/js/components/leaderboard.js
@@ -5,14 +5,14 @@ const Leaderboard = ({ leaderboard }) => {
 
     let mappedLeaderboard = leaderboard.map((user, i) => {
         return (
-                <li className="columns" key={ i }>
-                        <span className="rank-number column is-1">{ `${i + 9}` }</span>
-                        <div className="column is-7">
-                            <span className="subtitle is-4">{ user.username }</span>
-                        </div>
-                        <div className="column">
-                            <span className="subtitle is-4">{ user.average }</span>
-                        </div>
+                <li className="columns is-mobile" key={ i }>
+                    <span className="rank-number column is-1">{ `${i + 1}` }</span>
+                    <div className="column is-7">
+                        <span className="subtitle is-4">{ user.username }</span>
+                    </div>
+                    <div className="column">
+                        <span className="subtitle is-4">{ user.average }</span>
+                    </div>
                 </li>
         );
     });
@@ -21,9 +21,16 @@ const Leaderboard = ({ leaderboard }) => {
         <div className="leaderboard container average">
             <h1 className="has-text-centered">Leaderboard</h1>
             <div className="section">
-
-                <ol className="">
-                        { mappedLeaderboard }
+                <div className="leaderboard-header columns is-mobile">
+                    <div className="column is-7 is-offset-1">
+                        Nickname
+                    </div>
+                    <div className="column">
+                        Average score (%)
+                    </div>
+                </div>
+                <ol>
+                    { mappedLeaderboard }
                 </ol>
             </div>
         </div>
