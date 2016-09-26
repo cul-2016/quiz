89990cc8ec7006e46d3134bead00280000b12464
Mina <wilhelmina.jane@gmail.com>
2016-09-26 18:40:46 +0100

    animates trophies using greensock #146

--- src/js/components/student-module/result.js
+++ src/js/components/student-module/result.js
@@ -1,54 +1,66 @@
-import React, { PropTypes } from 'react';
+import React, { PropTypes, Component } from 'react';
 import { Link } from 'react-router';
 import classnames from 'classnames';
 import normaliseText from '../../lib/normaliseText';
 import showNavbar from '../../lib/showNavbar';
+import { elastic } from '../../lib/animate';
 
 
-const Result = ({ location, score, newTrophies }) => {
+export default class Result extends Component {
 
-    let module_id = location.pathname.split('/')[1];
-    newTrophies = newTrophies || [];
+    constructor (props) {
+        super(props);
+    }
 
-    let trophiesToPresent = newTrophies.map((trophy, i) => {
+    render () {
+
+        elastic('.trophy-container');
+
+        let { location, score, newTrophies } = this.props;
+
+        let module_id = location.pathname.split('/')[1];
+        newTrophies = newTrophies || [];
+
+        let trophiesToPresent = newTrophies.map((trophy, i) => {
+
+            return (
+                <div key={ i } className="column trophy-container">
+                    <div className="trophy" />
+                    <p>{ normaliseText(trophy) }</p>
+                </div>
+            );
+        });
+
+        let scoreClasses = classnames("title score", {
+            "animated bounceInUp": true,
+            "large": trophiesToPresent.length === 0,
+            "small": trophiesToPresent.length > 0
+        });
 
         return (
-            <div key={ i } className="column animated bounceInUp">
-                <div className="trophy" />
-                <p>{ normaliseText(trophy) }</p>
-            </div>
-        );
-    });
-
-    let scoreClasses = classnames("title score animated bounceInUp", {
-        "large": trophiesToPresent.length === 0,
-        "small": trophiesToPresent.length > 0
-    });
-
-    return (
-        <div className="result hero is-info is-bold is-fullheight">
-            <div className="hero-body">
-                <div className="container has-text-centered">
-                    <h2 className="subtitle">Your score is...</h2>
-                    <div className="columns is-mobile">
-                        { trophiesToPresent }
+            <div className="result hero is-info is-bold is-fullheight">
+                <div className="hero-body">
+                    <div className="container has-text-centered">
+                        <h2 className="subtitle">Your score is...</h2>
+                        <div className="columns is-mobile">
+                            { trophiesToPresent }
+                        </div>
+                        <h1 className={ scoreClasses }>
+                            { score }
+                        </h1>
                     </div>
-                    <h1 className={ scoreClasses }>
-                        { score }
-                    </h1>
+                </div>
+                <div className="hero-foot">
+                    <Link to={ `/${module_id}/student` }>
+                        <button onClick={ (e) => { showNavbar(e); } } className="button is-large is-success is-fullwidth">
+                            Finish
+                        </button>
+                    </Link>
                 </div>
             </div>
-            <div className="hero-foot">
-                <Link to={ `/${module_id}/student` }>
-                    <button onClick={ (e) => { showNavbar(e); } } className="button is-large is-success is-fullwidth">
-                        Finish
-                    </button>
-                </Link>
-            </div>
-        </div>
-    );
-
-};
+        );
+    }
+}
 
 Result.propTypes = {
     location: PropTypes.object,
---
+++ src/js/lib/animate.js
@@ -0,0 +1,18 @@
+import 'gsap';
+/* eslint-disable no-undef */
+
+export function elastic (selector) {
+
+
+    TweenMax.staggerFrom(
+        selector,
+        0.5,
+        {
+            y: 150,
+            opacity: 0,
+            ease: Back.easeOut.config(2, 0.5),
+            delay: 1.5, scale: 0
+        },
+        0.6
+    );
+}
