75d7ce7eb475c6af6e46a337c3f28767fcb06d2a
franzmoro <franzmoro88@gmail.com>
2016-12-16 16:21:19 +0100

    --amend

--- server/handlers/lecturer-verification.js
+++ server/handlers/lecturer-verification.js
@@ -4,7 +4,8 @@ var email   = require('sendemail'); // no api key
 var person = {
     name: "franzmoro88",
     email: "franzmoro88@gmail.com",
-    subject: "Welcome to City Uni :)"
+    subject: "Welcome to City Uni :)",
+    link: 'http://cul-app.herokuapp.com/'
 };
 
 email('lecturer-verification', person, function (error, result){
