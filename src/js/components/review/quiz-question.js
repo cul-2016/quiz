c91785dc0d868ddf5d64873841ce4ee265650c92
franzmoro <franzmoro88@gmail.com>
2017-01-03 11:33:22 +0100

    reveal answer button
    question answer will not be revealed to student unless clicking on button
    
    related #260

--- src/js/components/review/quiz-question.js
+++ src/js/components/review/quiz-question.js
@@ -1,25 +1,36 @@
 import React, { PropTypes } from 'react';
 import classnames from 'classnames';
 
-const Question = ({ idx, question, is_lecturer }) => {
+const Question = ({ idx, question, is_lecturer, showAnswer }) => {
+    const { isAnswerShowing } = question;
     let aClasses = classnames("column answer box", {
-        "correct_answer": 'a' === question.correct_answer.toLowerCase()
+        "correct_answer": isAnswerShowing &&
+            'a' === question.correct_answer.toLowerCase()
     });
     let bClasses = classnames("column answer box", {
-        "correct_answer": 'b' === question.correct_answer.toLowerCase()
+        "correct_answer": isAnswerShowing &&
+            'b' === question.correct_answer.toLowerCase()
     });
     let cClasses = classnames("column answer box", {
         "display-none": question.c === undefined,
-        "correct_answer": 'c' === question.correct_answer.toLowerCase()
+        "correct_answer": isAnswerShowing &&
+            'c' === question.correct_answer.toLowerCase()
     });
     let dClasses = classnames("column answer box", {
         "display-none": question.d === undefined,
-        "correct_answer": 'd' === question.correct_answer.toLowerCase()
+        "correct_answer": isAnswerShowing &&
+            'd' === question.correct_answer.toLowerCase()
     });
 
     return (
         <div key={idx} className="question">
-            <h4 className="title is-5 has-text-centered">{ `${idx + 1}. ${question.question}` }</h4>
+            <h4 className="title is-5 has-text-centered">
+                { `${idx + 1}. ${question.question}` }
+                { !isAnswerShowing &&
+                    <button onClick={ () => showAnswer(idx) }>Show Answer</button>
+                }
+            </h4>
+
 
             <div className="columns">
                 <div className={ aClasses }>
@@ -58,7 +69,8 @@ const Question = ({ idx, question, is_lecturer }) => {
 Question.propTypes = {
     idx: PropTypes.number.isRequired,
     is_lecturer: PropTypes.bool.isRequired,
-    question: PropTypes.object.isRequired
+    question: PropTypes.object.isRequired,
+    showAnswer: PropTypes.func
 };
 
 export default Question;
