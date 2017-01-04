4329a9c2bbaf5ab132acc59573ce4de5d1d7b5c7
franzmoro <franzmoro88@gmail.com>
2017-01-04 17:38:22 +0100

    quizzes recap modify for surveys
    related #263

--- src/js/components/module/module.js
+++ src/js/components/module/module.js
@@ -1,10 +1,10 @@
 import React, { PropTypes } from 'react';
 import Details from './details';
 import Quizzes from './quizzes';
+const  Surveys = Quizzes;
 import Spinner from '../general/spinner';
 
-
-const Module = ({ location, module, quizzes, isFetchingModule, sendQuizInvite }) => {
+const Module = ({ location, module, quizzes, surveys, isFetchingModule, sendQuizInvite }) => {
 
     return (
         <div>
@@ -22,8 +22,13 @@ const Module = ({ location, module, quizzes, isFetchingModule, sendQuizInvite })
                              trophies={ module.trophies }
                              medals={ module.medals }/>
 
-
                     <Quizzes quizzes={ quizzes }
+                        location={ location }
+                        sendQuizInvite={ sendQuizInvite }
+                        module_id={ module.module_id }/>
+
+                    <Surveys quizzes={ surveys }
+                        isSurvey={ true }
                         location={ location }
                         sendQuizInvite={ sendQuizInvite }
                         module_id={ module.module_id }/>
@@ -38,8 +43,13 @@ Module.propTypes = {
     location: PropTypes.object.isRequired,
     module: PropTypes.object,
     quizzes: PropTypes.array,
+    surveys: PropTypes.array,
     isFetchingModule: PropTypes.bool.isRequired,
     sendQuizInvite: PropTypes.func.isRequired
+};
+
+Module.defaultProps = {
+    surveys: []
 };
 
 export default Module;
