a9ee2592831d21c13b6e7fa57e681dfebbbbb8cf
franzmoro <franzmoro88@gmail.com>
2017-01-04 17:59:04 +0100

    small refactor

--- src/js/components/new-quiz/new-quiz.js
+++ src/js/components/new-quiz/new-quiz.js
@@ -4,24 +4,29 @@ import Questions from './questions';
 import classnames from 'classnames';
 
 
-const NewQuiz = ({ newQuiz, handleAddQuestion, handleDeleteQuestion, handleInputChange, handleQuizNameChange, handleSaveQuiz, handleIsLastQuiz, location, params }) => {
+const NewQuiz = ({
+    newQuiz: { name, questions, is_last_quiz, isSavingQuiz },
+    handleAddQuestion,
+    handleDeleteQuestion,
+    handleInputChange,
+    handleQuizNameChange,
+    handleSaveQuiz,
+    handleIsLastQuiz,
+    location,
+    params
+}) => {
 
-    const questionsValidation = newQuiz.questions.map((question) => {
-        if (!question.question || !question.a || !question.b || !question.correct_answer){
-            return false;
-        } else {
-            return true;
-        }
-    }).every((elem) => {
-        return elem;
-    });
+    const questionsValidation = questions.map((questionObj) => {
+        const { question, a, b, correct_answer } = questionObj;
+        return Boolean(question && a && b && correct_answer);
+    }).every((elem) => elem);
 
     const submitClasses = classnames("button is-success save-question", {
-        "is-disabled": !newQuiz.name || questionsValidation === false,
-        "is-loading": newQuiz.isSavingQuiz
+        "is-disabled": !name || questionsValidation === false,
+        "is-loading": isSavingQuiz
     });
     const quizNameClasses = classnames("help is-danger", {
-        "display-none": newQuiz.name
+        "display-none": name
     });
 
     return (
@@ -38,45 +43,50 @@ const NewQuiz = ({ newQuiz, handleAddQuestion, handleDeleteQuestion, handleInput
                 </div>
                 <div className="columns">
                     <div className="column is-5 is-offset-3 has-text-centered">
-                        <label className="label">
-                        New Quiz name
-                        </label>
+                        <label className="label">New Quiz name</label>
                         <input
                             className="input"
                             type="text"
-                            defaultValue={ newQuiz.name }
+                            defaultValue={ name }
                             onChange={ (e) => handleQuizNameChange(e.target.value) }
-                            placeholder='Quiz Name'></input>
-                            <span className={ quizNameClasses }>Please enter a Quiz Name</span>
+                            placeholder='Quiz Name'
+                            />
+                            <span className={ quizNameClasses }>
+                                Please enter a Quiz Name
+                            </span>
                     </div>
                     <div className="column is-1 has-text-centered">
-                        <label className="label">
-                        Last Quiz?
-                        </label>
+                        <label className="label">Last Quiz?</label>
                         <input
                             className="column is-1"
                             type="checkbox"
-                            checked={ newQuiz.is_last_quiz === true }
+                            checked={ is_last_quiz === true }
                             name="is_last_quiz"
-                            onClick={ handleIsLastQuiz } />
+                            onClick={ handleIsLastQuiz }
+                            />
                     </div>
                 </div>
 
-
                 <Questions
-                    questions={ newQuiz.questions }
+                    questions={ questions }
                     handleInputChange={ handleInputChange }
-                    handleDeleteQuestion={ handleDeleteQuestion } />
+                    handleDeleteQuestion={ handleDeleteQuestion }
+                    />
 
                 <div className="column is-6 is-offset-3 has-text-centered">
                     <button className="button is-info add-question" onClick={ handleAddQuestion }>
                         Add Question
                     </button>
-                    <button className={ submitClasses } onClick={ () => handleSaveQuiz(location.pathname.split('/')[1], newQuiz.name, newQuiz.questions, newQuiz.is_last_quiz) }>
+                    <button className={ submitClasses }
+                        onClick={ () => handleSaveQuiz(
+                            location.pathname.split('/')[1],
+                            name,
+                            questions,
+                            is_last_quiz
+                        ) }>
                         Save and Exit
                     </button>
                 </div>
-
             </div>
 
     );
