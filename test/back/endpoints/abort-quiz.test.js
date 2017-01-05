f90ce6ca96d0796f104ac18b644944469e23bf19
Sam Houston <shouston3@me.com>
2017-01-05 18:38:19 +0000

    implementing a auth test for the abort-quiz endpoint

--- test/back/endpoints/abort-quiz.test.js
+++ test/back/endpoints/abort-quiz.test.js
@@ -1,41 +1,44 @@
 const test = require('tape');
-const { server, testClient } = require('../../utils/init');
+const server = require('../../utils/initServer.js');
+const simulateAuth = require('../../utils/simulateAuth.js')(server);
+const pool = require('../../../server/lib/dbClient.js');
+const redisCli = server.app.redisCli;
+const initDb = require('../../utils/initDb.js')(pool, redisCli);
 
 test('`abort-quiz` endpoint works', (t) => {
 
     t.plan(2);
-    const quiz_id = 8;
+    initDb()
+    .then(() => simulateAuth())
+    .then((token) => {
+        const quiz_id = 8;
 
-    const options = {
-        method: 'GET',
-        url: `/abort-quiz?quiz_id=${quiz_id}`
-    };
+        const options = {
+            method: 'GET',
+            url: `/abort-quiz?quiz_id=${quiz_id}`,
+            headers: { Authorization: token }
+        };
 
-    server.inject(options, (response) => {
+        server.inject(options, (response) => {
 
-        t.equal(response.statusCode, 200, '200 status code');
-        t.ok(response.result, 'Get data back');
+            t.equal(response.statusCode, 200, '200 status code');
+            t.ok(response.result, 'Get data back');
+        });
     });
 });
 
-test('`abort-quiz` endpoint returns error if quiz_id is undefined', (t) => {
+test('`abort-quiz` endpoint returns 401 if authorization header isn\'t provided', (t) => {
 
     t.plan(1);
 
     server.inject('/abort-quiz', (response) => {
-        t.equal(response.statusCode, 500, '500 status code');
+        t.equal(response.statusCode, 401, '401 status code');
     });
 });
 
-
-test('---- adding the response back to the db that has been deleted ------', (t) => {
-
-    testClient.connect((error, client, done) => {
-        if (error) {
-            console.error(error, 'error from adding response back to db');
-        }
-        client.query('INSERT INTO responses (user_id, quiz_id, question_id, response) VALUES (24, 8, 31, b)');
-        done();
-        t.end();
-    });
+test.onFinish(() => {
+    redisCli.quit();
+    pool.end();
 });
+
+
