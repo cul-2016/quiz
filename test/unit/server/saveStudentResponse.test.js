27226707f68070481927eb79f95eccd85ae56d6d
franzmoro <franzmoro88@gmail.com>
2017-01-05 12:05:20 +0100

    use t.error in case of error
    instead of equal check of null

---
+++ test/unit/server/saveStudentResponse.test.js
@@ -0,0 +1,24 @@
+import test from 'tape';
+import { testClient } from '../../utils/init';
+import saveStudentResponse from '../../../server/lib/saveStudentResponse';
+
+test('`saveStudentResponse` works', (t) => {
+
+    t.plan(2);
+
+    const expectedCommand = 'INSERT';
+    const user_id = 1;
+    const quiz_id = 1;
+    const question_id = 1;
+    const response = 'c';
+
+    saveStudentResponse(testClient, user_id, quiz_id, question_id, response, (error, response) => {
+        if (error) {
+            console.error(error);
+            t.error('should not have errored');
+        } else {
+            t.pass('error is null, response is saved to db correctly.');
+            t.deepEqual(response.command, expectedCommand, 'Correct command of INSERT, response is saved to db correctly');
+        }
+    });
+});
