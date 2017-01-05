14771308f68ec3900c8105719df7f4ddfdfa3234
franzmoro <franzmoro88@gmail.com>
2017-01-05 12:30:47 +0100

    apply destructuring & naming clarity

--- server/handlers/save-student-response.js
+++ server/handlers/save-student-response.js
@@ -5,15 +5,15 @@ module.exports = {
     method: 'POST',
     path: '/save-student-response',
     handler: (request, reply) => {
-        var user_id = request.payload.user_id;
-        var quiz_id = request.payload.quiz_id;
-        var question_id = request.payload.question_id;
-        var response = request.payload.response;
-        if (user_id !== undefined && quiz_id !== undefined && question_id !== undefined && response !== undefined) {
+        let {
+            user_id, quiz_id, question_id, response: studentResponse
+        } = request.payload;
+
+        if (user_id !== undefined && quiz_id !== undefined && question_id !== undefined && studentResponse !== undefined) {
             user_id = parseInt(user_id);
             quiz_id = parseInt(quiz_id);
             question_id = parseInt(question_id);
-            saveStudentResponse(client, user_id, quiz_id, question_id, response, (error, response) => {
+            saveStudentResponse(client, user_id, quiz_id, question_id, studentResponse, (error, response) => {
                 var verdict = error || response;
                 reply(verdict);
             });
