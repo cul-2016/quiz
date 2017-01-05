572c048d47ac0fc923f23af742a1a8ed3c32c751
Sohil Pandya <sohilpandya@me.com>
2017-01-05 18:04:03 +0000

    refactored more db tests

--- test/back/db/getStudentHistory.test.js
+++ test/back/db/getStudentHistory.test.js
@@ -1,20 +1,28 @@
 const test = require('tape');
 const getStudentHistory = require('../../../server/lib/getStudentHistory');
-const { pool } = require('../../utils/init');
 const expected = require('../../utils/data-fixtures').studentHistoryData;
+const pool = require('../../../server/lib/dbClient.js');
+const redisCli = require('../../utils/configureRedis.js');
+const initDb = require('../../utils/initDb.js')(pool, redisCli);
 
 test('`getStudentHistory` works', (t) => {
 
     t.plan(1);
 
-    const user_id = 8;
-    const module_id = 'CENT';
-
-    getStudentHistory(pool, user_id, module_id, (error, response) => {
-
-        if (error) {
-            console.error(error);
-        }
-        t.deepEqual(response, expected, 'database returns student history');
+    initDb()
+    .then(() => {
+        const user_id = 8;
+        const module_id = 'CENT';
+        getStudentHistory(pool, user_id, module_id, (error, response) => {
+            if (error) {
+                console.error(error);
+            }
+            t.deepEqual(response, expected, 'database returns student history');
+        });
     });
+});
+
+test.onFinish(() => {
+    redisCli.quit();
+    pool.end();
 });
