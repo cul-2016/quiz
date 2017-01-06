47253a2ea2994b9200c8d253fa3d2e1fdad88857
Sam Houston <shouston3@me.com>
2017-01-06 16:20:39 +0000

    changing naming of fixtures for easier requiring, adding more endpoint tests

--- test/back/endpoints/runner.js
+++ test/back/endpoints/runner.js
@@ -1,4 +1,7 @@
-require('./modules.test.js');
-require('./users.test.js');
-require('./quizes.test.js');
-require('./index.test.js');
+// require('./modules.test.js');
+// require('./users.test.js');
+// require('./quizes.test.js');
+// require('./index.test.js');
+
+require('./endpoints.test.js');
+// require('./auth.test.js');
