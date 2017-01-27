64e527c5692ff8c7998d66461d6fce7be0709b6f
Sam Houston <shouston3@me.com>
2017-01-27 11:21:43 +0000

    implementing css in the register view

--- src/js/containers/signup.js
+++ src/js/containers/signup.js
@@ -1,6 +1,6 @@
 import { connect } from 'react-redux';
 import Signup from '../components/signup';
-import { updateInputField, registeringUser } from '../actions/register';
+import { updateInputField, registeringUser } from '../actions/register.js';
 
 const mapStateToProps = (state) => ({
     register: state.register
