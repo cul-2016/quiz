72845ce1f042ed9db4949ef31a8479bc07a6fb33
sohilpandya <sohilpandya@me.com>
2017-02-15 11:41:58 +0000

    added CSS for verifying lecturer section

--- src/js/components/email-verification/verify-email-message.js
+++ src/js/components/email-verification/verify-email-message.js
@@ -3,10 +3,17 @@ import { Link } from 'react-router';
 
 const VerficationMessageComponent = () => {
     return (
-        <div>
-            <p>Thank you for registering</p>
-            <p>Please follow the link we provided on your email to activate your account</p>
-            <Link to="/" className="button is-warning">Back to Home</Link>
+        <div className="login">
+            <div className="content__body">
+              <div className="header">
+                  <h1 className="f-headline"><img src="/Yellow.svg"></img></h1>
+                  <h3 className="f-headline"> Thank you for registering. Please follow the link we provided on your email to activate your account</h3>
+                  <Link className="button button__primary" to="/">
+                    <p className="f-subheader">Log in</p>
+                  </Link>
+              </div>
+            </div>
+
         </div>
     );
 };
