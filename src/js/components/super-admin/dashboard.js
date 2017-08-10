d768bf6805d6bc40213761ca249abb8e76fb3177
Jack Carlisle <jackcarlisle@hotmail.co.uk>
2017-08-10 10:48:21 +0100

    refactors the user tables and the migration file #465

--- src/js/components/super-admin/dashboard.js
+++ src/js/components/super-admin/dashboard.js
@@ -1,5 +1,5 @@
 import React, { PropTypes } from 'react';
-
+import UserTable from './user-table';
 
 const SuperAdminDashboard = ({
     students,
@@ -7,58 +7,10 @@ const SuperAdminDashboard = ({
     handleDeleteUser
 }) => {
 
-    const studentsTable = students.map((student, i) => {
-        return (
-            <tr className={`leaderboard__row--${i % 2 === 0 || i === 0 ? 'even' : 'odd'}` }>
-                <td className="f-body"> { student.email } </td>
-                <td className="f-body"> { student.username } </td>
-                <td><button className="button module__button button__tertiary" onClick={() => { handleDeleteUser(student.user_id); }}>Delete</button></td>
-            </tr>
-        );
-    });
-    const lecturersTable = lecturers.map((lecturer, i) => {
-        return (
-            <tr className={`leaderboard__row--${i % 2 === 0 || i === 0 ? 'even' : 'odd'}` }>
-                <td className="f-body"> { lecturer.email } </td>
-                <td className="f-body"> { lecturer.username } </td>
-                <td><button className="button module__button button__tertiary" onClick={() => { handleDeleteUser(lecturer.user_id); }}>Delete</button></td>
-            </tr>
-        );
-    });
-
-
     return (
       <div className="leaderboard">
-      <div className="content__body">
-          <h1 className="f-headline">Lecturers</h1>
-          <table>
-              <thead>
-                  <tr>
-                      <th className="f-body f-body--white">Email</th>
-                      <th className="f-body f-body--white">Username</th>
-                      <th className="f-body f-body--white"></th>
-                  </tr>
-              </thead>
-              <tbody>
-                { lecturersTable }
-              </tbody>
-            </table>
-        </div>
-        <div className="content__body">
-            <h1 className="f-headline">Students</h1>
-            <table>
-                <thead>
-                    <tr>
-                        <th className="f-body f-body--white">Email</th>
-                        <th className="f-body f-body--white">Username</th>
-                        <th className="f-body f-body--white"></th>
-                    </tr>
-                </thead>
-                <tbody>
-                  { studentsTable }
-                </tbody>
-              </table>
-          </div>
+        <UserTable users={ lecturers } title={ "lecturers" } handleDeleteUser={ handleDeleteUser } />
+        <UserTable users={ students } title={ "students" } handleDeleteUser={ handleDeleteUser } />
       </div>
     );
 };
