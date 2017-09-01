1acebe2f3c6be88ac6c8c3c17002b9c6e877fe28
sohilpandya <sohilpandya@me.com>
2017-09-01 14:23:58 +0100

    styled the results page for students for when they receive a badge and when they don't receive a badge related to #534

--- src/js/lib/animate.js
+++ src/js/lib/animate.js
@@ -7,17 +7,28 @@
 
 export function elastic (selector) {
 
-    TweenMax.staggerFrom(
-        selector,
-        0.5,
-        {
-            y: 150,
-            opacity: 0,
-            ease: Back.easeOut.config(2, 0.5),
-            delay: 1.5, scale: 0
-        },
-        0.8
-    );
+    // TweenMax.staggerFrom(
+    //     selector,
+    //     0.5,
+    //     {
+    //         y: 150,
+    //         opacity: 0,
+    //         ease: Back.easeOut.config(2, 0.5),
+    //         delay: 1.5, scale: 0
+    //     },
+    //     0.8
+    // );
+
+    // FOR TWO BADGES
+    const tl = new TimelineMax();
+
+
+    tl.add(TweenMax.to(selector, 0, { x: 100, y: 250 }));
+    tl.add(TweenMax.staggerFromTo(selector, 1.5, { scale: 0, rotation: 0 }, { scale: 1, opacity: 1, rotation: 1440, ease: Back.easeOut.config(2, 0.5) }, 0.5));
+    tl.add(TweenMax.to(selector, 1, {  }));
+    tl.add(TweenMax.to(selector, 1, { x: 10, y: 10, scale: 1 }));
+
+
 }
 
 /**
@@ -27,9 +38,14 @@ export function elastic (selector) {
 
 export function fadeOutThenIn (selector) {
 
+    console.log(selector);
     const tl = new TimelineMax();
-    tl.add(TweenMax.to(selector, 0.3, { ease: Circ.easeOut, opacity: 0 }));
-    tl.add(TweenMax.to(selector, 0.3, { ease: Circ.easeIn, opacity: 1, backgroundColor: '#42afe3' }));
+    console.log(tl);
+    tl.add(TweenMax.to(selector, 0, { ease: Circ.easeOut, opacity: 0 }));
+    tl.add(TweenMax.to(selector, 0.3, { y: 1000 }));
+    tl.add(TweenMax.to(selector, 0.3, { ease: Circ.easeIn, opacity: 1 }));
+    tl.add(TweenMax.to(selector, 0.3, { y: 100 }));
+    tl.add(TweenMax.to(selector, 0.3, { ease: Circ.easeIn, opacity: 1 }));
 }
 
 /**
