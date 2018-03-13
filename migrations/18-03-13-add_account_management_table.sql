3f5f4790b04505657f1e5035871882a119c8817c
sohilpandya <sohilpandya@me.com>
2018-03-13 17:39:09 +0000

    created a new table called account_management, related to #620

---
+++ migrations/18-03-13-add_account_management_table.sql
@@ -0,0 +1,13 @@
+CREATE TYPE account_types AS ENUM ('group admin', 'group lecturer', 'individual lecturer');
+
+/* account_management */
+CREATE TABLE IF NOT EXISTS account_management (
+    account_management_id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    institution TEXT NOT NULL,
+    department TEXT NOT NULL,
+    account_type account_types,
+    paid BOOLEAN DEFAULT FALSE NOT NULL,
+    code TEXT
+);
