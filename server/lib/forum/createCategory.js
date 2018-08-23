941563c0a30477cce1947e4e1feff7edb5e6c5f2
Danielwhyte <danwhy@gmail.com>
2018-08-23 15:48:43 +0100

    removes default privileges for modules, #690

--- server/lib/forum/createCategory.js
+++ server/lib/forum/createCategory.js
@@ -1,11 +1,37 @@
 const addPrivileges = require('./addPrivileges.js');
+const deletePrivileges = require('./deletePrivileges.js');
 
 module.exports = function (axios, name, id, cb) {
+  let cid;
   axios.post(`${process.env.FORUM_URL}/api/v2/categories`, {
     name: name
   })
   .then((res) => {
-    return addPrivileges(axios, res.data.payload.cid, [id], ['moderate'], cb);
+    cid = res.data.payload.cid;
+    return addPrivileges(axios, cid, [id], ['moderate']);
+  })
+  .then((res) => {
+    return deletePrivileges(axios, cid, [
+      'registered-users',
+      'guests',
+      'spiders'
+    ], [
+      'groups:find',
+      'groups:read',
+      'groups:topics:read',
+      'groups:topics:create',
+      'groups:topics:reply',
+      'groups:topics:tag',
+      'groups:posts:edit',
+      'groups:posts:history',
+      'groups:posts:delete',
+      'groups:posts:upvote',
+      'groups:posts:downvote',
+      'groups:topics:delete',
+      'groups:posts:view_deleted',
+      'groups:purge',
+      'groups:moderate'
+    ], cb);
   })
   .catch(err => {
     return cb(err);
--- server/lib/forum/deletePrivileges.js
+++ server/lib/forum/deletePrivileges.js
@@ -1,7 +1,9 @@
 module.exports = function (axios, cid, users, privileges, cb) {
-  axios.put(`${process.env.FORUM_URL}/api/v2/categories/${cid}/privileges`, {
-    privileges: privileges,
-    groups: users
+  axios.delete(`${process.env.FORUM_URL}/api/v2/categories/${cid}/privileges`, {
+    data: {
+      privileges: privileges,
+      groups: users
+    }
   })
   .then((res) => {
     if (cb) {
